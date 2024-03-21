5db48e7cc7cd528938de7887aa13d6431eb1cd9b
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-03-21 14:35:12 -0300

    Karafka web UI

---
+++ ruby-apps/dashboard/config.ru
@@ -0,0 +1,2 @@
+require_relative 'karafka.rb'
+run Karafka::Web::App
---
+++ ruby-apps/dashboard/karafka.rb
@@ -0,0 +1,28 @@
+ENV['KARAFKA_ENV'] ||= 'development'
+Bundler.require(:default, ENV['KARAFKA_ENV'])
+
+APP_LOADER = Zeitwerk::Loader.new
+APP_LOADER.enable_reloading
+
+APP_LOADER.setup
+APP_LOADER.eager_load
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    # list of all options:
+    # > https://github.com/karafka/karafka/blob/master/lib/karafka/setup/config.rb
+    # > https://karafka.io/docs/Librdkafka-Configuration/
+    config.kafka = {
+      'bootstrap.servers': 'kafka:9092',
+      'allow.auto.create.topics': false
+    }
+    config.client_id = 'ruby_consumer_group_app'
+  end
+end
+
+Karafka::Web.setup do |config|
+  # You may want to set it per ENV. This value was randomly generated.
+  config.ui.sessions.secret = '82a905c85911e6da2faf02801fc4bd9bcff43cc0ee8320c0695d8e319bc76d23'
+end
+
+Karafka::Web.enable!
